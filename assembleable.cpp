d94b85101d25b84665b6890fd9335602bf2d2af2
Clownacy <Clownacy@users.noreply.github.com>
2024-02-06 16:57:54 +0000

    Add support for compressed IO.

--- assembleable.cpp
+++ assembleable.cpp
@@ -2,9 +2,16 @@
 
 #include <sstream>
 
+#include "comper.h"
+#include "kosinski.h"
+#include "kosplus.h"
+#include "nemesis.h"
+
 #include "assembler.h"
 
 namespace libsonassmd {
+
+// File IO
 
 std::ifstream Assembleable::createFileInputStream(const char* const file_path, const std::ios::openmode flags) const
 {
@@ -49,6 +56,84 @@ void Assembleable::toBinaryFile(const char* const file_path) const
 	auto stream = createFileOutputStream(file_path, std::ios::binary);
 	toBinaryStream(stream);
 }
+
+// Compressed IO
+
+void Assembleable::fromCompressedStream(std::istream &stream, bool (* const function)(std::istream &input, std::iostream &output))
+{
+	std::stringstream string_stream(std::ios::binary | std::ios::in | std::ios::out);
+	function(stream, string_stream);
+	fromBinaryStream(string_stream);
+}
+
+void Assembleable::fromComperStream(std::istream &stream)
+{
+	fromCompressedStream(stream, comper::decode);
+}
+
+void Assembleable::fromKosinskiStream(std::istream &stream)
+{
+	fromCompressedStream(stream, kosinski::decode);
+}
+
+void Assembleable::fromModuledKosinskiStream(std::istream &stream)
+{
+	fromCompressedStream(stream, [](std::istream &input, std::iostream &output){return kosinski::moduled_decode(input, output);});
+}
+
+void Assembleable::fromKosinskiPlusStream(std::istream &stream)
+{
+	fromCompressedStream(stream, kosplus::decode);
+}
+
+void Assembleable::fromModuledKosinskiPlusStream(std::istream &stream)
+{
+	fromCompressedStream(stream, [](std::istream &input, std::iostream &output){return kosplus::moduled_decode(input, output);});
+}
+
+void Assembleable::fromNemesisStream(std::istream &stream)
+{
+	fromCompressedStream(stream, [](std::istream &input, std::iostream &output){return nemesis::decode(input, output);});
+}
+
+void Assembleable::toCompressedStream(std::ostream &stream, bool (* const function)(std::istream &input, std::ostream &output))
+{
+	std::stringstream string_stream(std::ios::binary | std::ios::in | std::ios::out);
+	toBinaryStream(string_stream);
+	function(string_stream, stream);
+}
+
+void Assembleable::toComperStream(std::ostream &stream)
+{
+	toCompressedStream(stream, comper::encode);
+}
+
+void Assembleable::toKosinskiStream(std::ostream &stream)
+{
+	toCompressedStream(stream, kosinski::encode);
+}
+
+void Assembleable::toModuledKosinskiStream(std::ostream &stream)
+{
+	toCompressedStream(stream, [](std::istream &input, std::ostream &output){return kosinski::moduled_encode(input, output);});
+}
+
+void Assembleable::toKosinskiPlusStream(std::ostream &stream)
+{
+	toCompressedStream(stream, kosplus::encode);
+}
+
+void Assembleable::toModuledKosinskiPlusStream(std::ostream &stream)
+{
+	toCompressedStream(stream, [](std::istream &input, std::ostream &output){return kosplus::moduled_encode(input, output);});
+}
+
+void Assembleable::toNemesisStream(std::ostream &stream)
+{
+	toCompressedStream(stream, nemesis::encode);
+}
+
+// Assembler IO
 
 void Assembleable::fromAssemblyStream(std::istream &stream)
 {
--- assembleable.h
+++ assembleable.h
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <istream>
+#include <iostream>
 #include <ostream>
 
 namespace libsonassmd {
@@ -12,12 +13,27 @@ class Assembleable
 private:
 	std::ifstream createFileInputStream(const char *file_path, std::ios::openmode flags) const;
 	std::ofstream createFileOutputStream(const char *file_path, std::ios::openmode flags) const;
+	void fromCompressedStream(std::istream &stream, bool (*function)(std::istream &input, std::iostream &output));
+	void toCompressedStream(std::ostream &stream, bool (*function)(std::istream &input, std::ostream &output));
 
 public:
 	void fromAssemblyFile(const char *file_path);
 	void fromBinaryFile(const char *file_path);
 	void toAssemblyFile(const char *file_path) const;
 	void toBinaryFile(const char *file_path) const;
+
+	void fromComperStream(std::istream &stream);
+	void fromKosinskiStream(std::istream &stream);
+	void fromModuledKosinskiStream(std::istream &stream);
+	void fromKosinskiPlusStream(std::istream &stream);
+	void fromModuledKosinskiPlusStream(std::istream &stream);
+	void fromNemesisStream(std::istream &stream);
+	void toComperStream(std::ostream &stream);
+	void toKosinskiStream(std::ostream &stream);
+	void toModuledKosinskiStream(std::ostream &stream);
+	void toKosinskiPlusStream(std::ostream &stream);
+	void toModuledKosinskiPlusStream(std::ostream &stream);
+	void toNemesisStream(std::ostream &stream);
 
 	virtual void fromAssemblyStream(std::istream &stream);
 	virtual void fromBinaryStream(std::istream &stream) = 0;
