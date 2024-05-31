5224a56ecbd7d98722fa48aaae63fdb18dd4171f
Clownacy <Clownacy@users.noreply.github.com>
2024-05-31 17:47:29 +0100

    Switch to the C++ clownassembler wrapper.

--- assembler.cpp
+++ assembler.cpp
@@ -1,84 +1,11 @@
 #include "assembler.h"
 
 #include <cassert>
-#include <cstring>
 #include <sstream>
 
-#include "clownassembler/semantic.h"
+#include "clownassembler/cpp/clownassembler.h"
 
 namespace libsonassmd {
-
-struct InputCallbackData
-{
-	std::istream *input;
-	std::stringstream mapmacros;
-};
-
-struct OutputCallbackData
-{
-	std::ostream *output;
-	std::ostream::pos_type base_position;
-};
-
-static int ReadCharacter(std::istream &stream)
-{
-	const auto value = stream.get();
-
-	return stream.eof() ? -1 : value;
-}
-
-static int ReadCharacter(void* const user_data)
-{
-	InputCallbackData* const callback_data = static_cast<InputCallbackData*>(user_data);
-
-	const auto value = ReadCharacter(callback_data->mapmacros);
-
-	return value != -1 ? value : ReadCharacter(*callback_data->input);
-}
-
-static char* ReadLine(std::istream* const stream, char* const buffer, const size_t buffer_size)
-{
-	// TODO: This isn't the closest emulation of fgets...
-	stream->getline(buffer, buffer_size);
-
-	if (stream->fail())
-		return buffer[0] == '\0' ? nullptr : buffer;
-
-	buffer[std::strlen(buffer)] = '\n'; // TODO: Doesn't properly null-terminate.
-
-	return buffer;
-}
-
-static char* ReadLine(void* const user_data, char* const buffer, const size_t buffer_size)
-{
-	InputCallbackData* const callback_data = static_cast<InputCallbackData*>(user_data);
-
-	const auto value = ReadLine(&callback_data->mapmacros, buffer, buffer_size);
-
-	return value != nullptr ? value : ReadLine(callback_data->input, buffer, buffer_size);
-}
-
-static void Seek(void* const user_data, const size_t position)
-{
-	OutputCallbackData* const callback_data = static_cast<OutputCallbackData*>(user_data);
-
-	callback_data->output->seekp(callback_data->base_position);
-	callback_data->output->seekp(position, std::ios::cur);
-}
-
-static void WriteCharacter(void* const user_data, const int character)
-{
-	OutputCallbackData* const callback_data = static_cast<OutputCallbackData*>(user_data);
-
-	callback_data->output->put(character);
-}
-
-static void WriteCharacters(void* const user_data, const char* const characters, const size_t total_characters)
-{
-	OutputCallbackData* const callback_data = static_cast<OutputCallbackData*>(user_data);
-
-	callback_data->output->write(characters, total_characters);
-}
 
 static unsigned int GameToNumber(const Game game)
 {
@@ -168,37 +95,17 @@ s3kPlayerDplcEntry macro totalTiles, tileIndex
 
 bool Assemble(std::istream &input, std::ostream &output, const Game game)
 {
-	InputCallbackData input_callback_data = {
-		&input,
-		{}
-	};
-
-	ClownAssembler_TextInput input_callbacks = {
-		&input_callback_data,
-		ReadCharacter,
-		ReadLine
-	};
-
-	OutputCallbackData output_callback_data = {
-		&output,
-		output.tellp()
-	};
-
-	ClownAssembler_BinaryOutput output_callbacks = {
-		&output_callback_data,
-		WriteCharacter,
-		WriteCharacters,
-		Seek
-	};
-
-	EmitMapMacros(input_callback_data.mapmacros, game);
+	std::stringstream expanded_input;
+	EmitMapMacros(expanded_input, game);
+	// TODO: Is there any way to make a meta-stream that concatonates these two instead?
+	expanded_input << input.rdbuf();
 
 	// It's okay for some IO errors to occur here.
 	const auto original_exceptions = input.exceptions();
 	input.exceptions(original_exceptions & ~(std::ios::eofbit | std::ios::failbit));
 
 	// TODO: Real filename.
-	const bool success = ClownAssembler_Assemble(&input_callbacks, &output_callbacks, nullptr, nullptr, nullptr, "[Filename unknown]", cc_false, cc_false, cc_false, cc_false, cc_false, nullptr, nullptr);
+	const bool success = ClownAssembler::Assemble(expanded_input, output, nullptr, nullptr, nullptr, "[Filename unknown]", false, false, false, false, false, nullptr);
 
 	input.clear();
 	input.exceptions(original_exceptions);
