Clownacy/libsonassmd

commit 9bad152f93b66774343b4b045ab9e33ff40d5627

Show correct assembly filename and line in errors.

assembleable.cpp
@@ -14,10 +14,19 @@ namespace libsonassmd {
 
 void Assembleable::fromFile(const std::filesystem::path &file_path, const Format format)
 {
-	std::ifstream stream(file_path, format == Format::ASSEMBLY ? std::ios::in : std::ios::binary);
+	if (format == Format::ASSEMBLY)
+	{
+		std::stringstream string_stream(std::ios::in | std::ios::out | std::ios::binary);
+		Assemble(file_path, string_stream, game);
+		fromBinaryStream(string_stream);
+	}
+	else
+	{
+		std::ifstream stream(file_path, std::ios::binary);
 
-	stream.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
-	fromStream(stream, format);
+		stream.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
+		fromBinaryStream(stream);
+	}
 }
 
 void Assembleable::toFile(const std::filesystem::path &file_path, const Format format) const

assembler.cpp
@@ -93,26 +93,40 @@ s3kPlayerDplcEntry macro totalTiles, tileIndex
 )";
 };
 
+static void AssembleInternal(std::istream &input, std::ostream &output, const Game game)
+{
+	// TODO: Real filename.
+	std::stringstream errors;
+	const bool success = ClownAssembler::Assemble(input, output, &errors, nullptr, nullptr, "[Internal buffer]", false, false, false, false, false, nullptr);
+
+	if (!success)
+		throw std::runtime_error("Failed to assemble:\n\n" + errors.str());
+}
+
 void Assemble(std::istream &input, std::ostream &output, const Game game)
 {
 	std::stringstream expanded_input;
 	EmitMapMacros(expanded_input, game);
-	// TODO: Is there any way to make a meta-stream that concatenates these two instead?
-	expanded_input << input.rdbuf();
 
 	// It's okay for some IO errors to occur here.
 	const auto original_exceptions = input.exceptions();
 	input.exceptions(original_exceptions & ~(std::ios::eofbit | std::ios::failbit));
 
-	// TODO: Real filename.
-	std::stringstream errors;
-	const bool success = ClownAssembler::Assemble(expanded_input, output, &errors, nullptr, nullptr, "[Filename unknown]", false, false, false, false, false, nullptr);
+	// TODO: Is there any way to make a meta-stream that concatenates these two instead?
+	expanded_input << input.rdbuf();
 
 	input.clear();
 	input.exceptions(original_exceptions);
 
-	if (!success)
-		throw std::runtime_error("Failed to assemble:\n\n" + errors.str());
+	AssembleInternal(expanded_input, output, game);
+}
+
+void Assemble(const std::filesystem::path &path, std::ostream &output, const Game game)
+{
+	std::stringstream input;
+	EmitMapMacros(input, game);
+	input << "\tinclude " << path;
+	AssembleInternal(input, output, game);
 }
 
 }

assembler.h
@@ -1,6 +1,7 @@
 #ifndef LIBSONASSMD_ASSEMBLER
 #define LIBSONASSMD_ASSEMBLER
 
+#include <filesystem>
 #include <istream>
 #include <ostream>
 
@@ -9,6 +10,7 @@
 namespace libsonassmd {
 
 void Assemble(std::istream &input, std::ostream &output, Game game);
+void Assemble(const std::filesystem::path &path, std::ostream &output, Game game);
 
 }
 

commit f53e98f258796f89ac74ff2d36fc6c9a791915c4

Fix compressed data failing to load.
This was broken back in 9bad152f93b66774343b4b045ab9e33ff40d5627.

assembleable.cpp
@@ -24,7 +24,7 @@ void Assembleable::fromFile(const std::filesystem::path &file_path, const Format
 	else
 	{
 		std::ifstream stream(file_path, std::ios::binary);
-		fromBinaryStream(stream);
+		fromStream(stream, format);
 	}
 }
 