198029bac058cbac09d96afd1f7030dc0471aeb9
Clownacy <Clownacy@users.noreply.github.com>
2025-08-05 21:52:22 +0100

    Add a missing `#include`.

--- parser/lexical.h
+++ parser/lexical.h
@@ -1,6 +1,8 @@
 #ifndef LEXICAL_H
 #define LEXICAL_H
 
+#include <optional>
+
 // Work around some insane bullshit in 'FlexLexer.h' that causes a double-defined error.
 // I thought we left dogshit code like this in the past.
 #ifndef yyFlexLexer
